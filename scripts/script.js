abd3d8fa26fea5d07fce0b6107e827e6faa1562a
Ilya Kotkov <MopsBox2552@yandex.ru>
2022-12-12 18:05:17 +0300

    Добавить функцию удаления, лайка, добавить функцию появления карточек по умолчанию на странице

--- scripts/script.js
+++ scripts/script.js
@@ -27,3 +27,60 @@ function save(event) {
     closePopup();
 }
 popupForm.addEventListener('submit', save);
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const elementContent = document.querySelector(".elements");
+const elementTemplate = document.querySelector("#element__template").content;
+
+const elementInfo = initialCards.map(function (item) {
+  return {
+    name: item.name,
+    link: item.link
+  };
+});
+
+function render() {elementInfo.forEach(renderElement);}
+
+function renderElement({ name, link }) {
+  const element = elementTemplate.querySelector(".element").cloneNode(true);
+  element.querySelector(".element__title").textContent = name;
+  element.querySelector(".element__image").src = link;
+
+  element.querySelector('.element__deleteButton').addEventListener('click', () => {
+    element.remove();
+  });
+
+  element.querySelector('.element__likeButton').addEventListener('click', (e) => 
+   e.target.classList.toggle('element__likeButton_active')
+ ); 
+
+   elementContent.append(element);
+}
+
+render();
