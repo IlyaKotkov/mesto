c1b17e3942437325cc411e9196b0fc589114cf1e
Ilya Kotkov <MopsBox2552@yandex.ru>
2022-12-15 21:50:42 +0300

    Реализовать попап добавления карточки, исправить открытие и закрытие карточек, реализовать разметку попап добавления карточки

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,45 @@
-let openPopupButton = document.querySelector('.profile__editButton');
-let closePopupButton = document.querySelector('.popup__closed');
-let popup = document.querySelector('.popup');
-let namePopupInput = document.querySelector('.popup__input_type_name');
-let activityPopupInput = document.querySelector('.popup__input_type_job');
-let profileName = document.querySelector('.profile__name')
-let profileDescription = document.querySelector('.profile__description');
-let popupForm = document.querySelector('.popup__form');
+const openPopupButton = document.querySelector('.profile__editButton');
+const popup = document.querySelector('.popup');
+const namePopupInput = document.querySelector('.popup__input_type_name');
+const activityPopupInput = document.querySelector('.popup__input_type_job');
+const profileName = document.querySelector('.profile__name')
+const profileDescription = document.querySelector('.profile__description');
+const popupForm = document.querySelector('.popup__form');
+const openAddButton = document.querySelector('.profile__addButton');
+const popupCardForm = document.querySelector('.popup-card__form');
+const popupEdit = document.querySelector('.popup_type_edit');
+const popupAdd = document.querySelector('.popup_type_add');
+const closePopupButton = popupEdit.querySelector('.popup__closed');
+const closeAddPopupButton = popupAdd.querySelector('.popup__closed');
+const elementContent = document.querySelector(".elements");
+const elementTemplate = document.querySelector("#element__template").content;
+const popupFormAdd = popupAdd.querySelector('.popup__form');
 
-function openPopup() {
-    popup.classList.add('popup_opened');
+function openPopup(popup) {
+  popup.classList.add("popup_opened")
+}
+
+function closePopup(popup) {
+  popup.classList.remove("popup_opened")
+}
+
+openPopupButton.addEventListener('click', () => {
     namePopupInput.value = profileName.textContent;
     activityPopupInput.value = profileDescription.textContent;
-
-}
-openPopupButton.addEventListener('click', openPopup);
-
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
-closePopupButton.addEventListener('click', closePopup);
+    openPopup(popupEdit);
+})
 
 function save(event) {
-    event.preventDefault();
-    profileName.textContent = namePopupInput.value;
-    profileDescription.textContent = activityPopupInput.value;
-    closePopup();
+  event.preventDefault();
+  profileName.textContent = namePopupInput.value;
+  profileDescription.textContent = activityPopupInput.value;
+  closePopup(popupEdit);
 }
 popupForm.addEventListener('submit', save);
+
+closePopupButton.addEventListener('click', () => {
+  closePopup(popupEdit);
+})
 
 const initialCards = [
     {
@@ -55,9 +68,6 @@ const initialCards = [
     }
 ];
 
-const elementContent = document.querySelector(".elements");
-const elementTemplate = document.querySelector("#element__template").content;
-
 const elementInfo = initialCards.map(function (item) {
   return {
     name: item.name,
@@ -71,16 +81,29 @@ function renderElement({ name, link }) {
   const element = elementTemplate.querySelector(".element").cloneNode(true);
   element.querySelector(".element__title").textContent = name;
   element.querySelector(".element__image").src = link;
+  elementContent.append(element);
 
-  element.querySelector('.element__deleteButton').addEventListener('click', () => {
+  const trash = element.querySelector('.element__deleteButton').addEventListener('click', () => {
     element.remove();
   });
-
-  element.querySelector('.element__likeButton').addEventListener('click', (e) => 
+  
+  const like = element.querySelector('.element__likeButton').addEventListener('click', (e) => 
    e.target.classList.toggle('element__likeButton_active')
- ); 
-
-   elementContent.append(element);
+  ); 
 }
-
 render();
+
+openAddButton.addEventListener('click', () => {
+  openPopup(popupAdd);
+})
+
+function saveAddCard(event) {
+  event.preventDefault();
+
+  closePopup(popupAdd);
+}
+popupFormAdd.addEventListener('submit', saveAddCard);
+
+closeAddPopupButton.addEventListener('click', () => {
+  closePopup(popupAdd);
+})
