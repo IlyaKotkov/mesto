434a1ccadb313bdb161f09cc086cc40fba9428e1
Ilya Kotkov <MopsBox2552@yandex.ru>
2023-01-08 15:26:41 +0300

    Исправить ошибки найденные автотестами

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ configValidation = {
   submitButtonSelector: '.popup__submit',
   inactiveButtonClass: 'popup__submit_disabled',
   inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
+  errorClass: 'popup__error'
 }; 
 
 const profileEditForm = document.querySelector('#editForm')
